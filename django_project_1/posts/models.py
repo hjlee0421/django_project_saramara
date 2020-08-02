a704624e9f2547cb567f6d6c8dc2b71f58e86fc4
trend-following-investor <trend.following.investor@gmail.com>
2020-08-02 14:11:18 +0900

    defualt user with post model

--- django_project_1/posts/models.py
+++ django_project_1/posts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.conf import settings  # 추천!
+# from django.conf.auth.models import User  # 비추
 
 # Create your models here.
+
+
+class Post(models.Model):
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE)  # 추천!
+    title = models.CharField(max_length=128)
+    content = models.TextField()
+    # author = models.ForeignKey(User) 		# 비추
+    # author = models.ForeignKey('auth.User') # 비추
