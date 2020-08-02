d3eee204eadd9f44829067d81df30eb435fab634
trend-following-investor <trend.following.investor@gmail.com>
2020-08-02 16:27:13 +0900

    add polls app

---
+++ django_project_1/polls/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+appname = 'polls'
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+]
+
+# name=index 를 꼭 해줘야 하나??
