f896309965f310ecaa7d9964779d57e24732114d
trend-following-investor <hjlee0421@gmail.com>
2020-11-01 15:51:41 +0900

    django_project_1 을 django_project로 변경함

--- django_project/django_project/urls.py
+++ django_project/django_project/urls.py
@@ -1,4 +1,4 @@
-"""django_project_1 URL Configuration
+"""django_project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
