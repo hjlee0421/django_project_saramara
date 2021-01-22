8abd406f5dd0964b15fe8238babd839e16f24c6b
trend-following-investor <hjlee0421@gmail.com>
2021-01-22 23:47:05 +0900

    urls.py  에서 코멘트 처리된 부분 삭제함

--- django_project/posts/urls.py
+++ django_project/posts/urls.py
@@ -4,28 +4,20 @@ from . import views
 
 app_name = 'posts'
 
+# view.함수명 인 url들 모두 class based view 로 변경한 후 views.클래스명.as_view() 로 변경하기
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='index'),
-    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
-    # path('<int:pk>/add_comment/', views.DetailView.as_view(), name="add_comment"),
-    # path('<int:pk>/sara/', views.DetailView.as_view(), name="sara_button"),
-    # path('<int:pk>/mara/', views.DetailView.as_view(), name="mara_button"),
-    path('<int:pk>/edit/', views.EditView.as_view(), name='edit'),
-    path('mypage/', views.MypageView.as_view(), name='mypage'),
-    path('ask/', views.AskView.as_view(), name='ask'),
-    # path('ask/<int:pk>/', views.EditView.as_view(), name='edit'),
-    path('kakao_login/', views.kakao_login, name="kakao_login"),
-    path('kakao_logout/', views.kakao_logout, name="kakao_logout"),
-    path('kakao_unlink/', views.kakao_unlink, name='kakao_unlink'),
-    # path('user_info/', views.user_info, name='user_info'),
-    # path('UploadImage/', views.UploadImage, name='UploadImage'),
-    # path('signup/', views.SignupView.as_view(), name='signup'),
-    # path('signin/', views.SigninView.as_view(), name='signin'),
-    # path('signout/', views.SignoutView.as_view(), name='signout'),
-    # path('test/', views.TestIndexView.as_view(), name='testindex'),
-    # path('testask/', views.TestAskView.as_view(), name='testask'),
+    path('', views.IndexView.as_view(), name='index'),  # 메인 인데스
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),  # 포스트 디테일
+    path('<int:pk>/edit/', views.EditView.as_view(), name='edit'),  # 포스트 디테일 > 편집
+    path('mypage/', views.MypageView.as_view(), name='mypage'),  # 마이페이지
+    path('ask/', views.AskView.as_view(), name='ask'),  # 질문하기
+    path('kakao_login/', views.kakao_login, name="kakao_login"),  # 카카오 로그인
+    path('kakao_logout/', views.kakao_logout, name="kakao_logout"),  # 카카오 로그아웃
+    path('kakao_unlink/', views.kakao_unlink, name='kakao_unlink'),  # 카카오 연동해제
+    # 프로필이미지 & 닉네임 변경 url 주소 이해하기 쉽게 변경작업 해야함
     path('upload_image/', views.upload_image, name="upload_image"),
+    # 프로필 변경할때 사진부분 서버로 넘기는 작업처리
     path('add_image/', views.addImage_view, name="add_image"),
+    # 프로필 변경할때 사진부분 클라이언트로 가져오는 작업처리
     path('get_images/', views.getImages_view, name="get_images"),
-
 ]
