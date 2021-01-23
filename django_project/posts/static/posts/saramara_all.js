ffa4e1694379acac02662ff4129908ddb998e52f
trend-following-investor <hjlee0421@gmail.com>
2021-01-23 21:58:29 +0900

    검색설정 toggle은 index에 둬야 함

--- django_project/posts/static/posts/saramara_all.js
+++ django_project/posts/static/posts/saramara_all.js
@@ -1,38 +1,3 @@
-
-/* Close the dropdown menu if the user clicks outside of it */
-// window.onclick = function (event) {
-//   if (!event.target.matches(".dropbtn")) {
-//     var dropdowns = document.getElementsByClassName("dropdown-content");
-//     var i;
-//     for (i = 0; i < dropdowns.length; i++) {
-//       var openDropdown = dropdowns[i];
-//       if (openDropdown.classList.contains("show")) {
-//         openDropdown.classList.remove("show");
-//       }
-//     }
-//   }
-// };
-
-// var acc = document.getElementsByClassName("accordion");
-// var i;
-
-// for (i = 0; i < acc.length; i++) {
-//   acc[i].addEventListener("click", function () {
-//     /* Toggle between adding and removing the "active" class,
-//     to highlight the button that controls the panel */
-//     this.classList.toggle("active");
-
-//     /* Toggle between hiding and showing the active panel */
-//     var panel = this.nextElementSibling;
-//     if (panel.style.display === "block") {
-//       panel.style.display = "none";
-//     } else {
-//       panel.style.display = "block";
-//     }
-//   });
-// }
-
-
 // #####################################################################
 // 카카오계정 관련 함수들
 // #####################################################################
@@ -179,16 +144,6 @@ $(function () {
 // #####################################################################
 // 포스트 수정 관련 함수들
 // #####################################################################
-
-
-
-
-$(document).ready(function () {
-  $("#search-button").click(function () {
-    $("div.items-sort-all").toggle();
-  });
-});
-
 
 
 // var slideIndex = 1;
